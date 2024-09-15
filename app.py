439d83096aefc1352681cef93ee5f9534928e9da
Nhan Pham Thanh <ptnhanit230104@gmail.com>
2024-09-15 22:51:56 +0700

    Upload home.html and app.py files

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request
 from summarize import summarize
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return render_template('home.html')
+    file = request.files['file']
+    if file.filename == '':
+        return render_template('home.html')
+    if file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
     return render_template('home.html')
 
 @app.route('/summarize', methods=['POST'])
